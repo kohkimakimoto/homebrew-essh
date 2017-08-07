204e503a446d574687dac18159e9b9df1a02e323
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-08-07 14:51:30 +0900

    update version

--- essh.rb
+++ essh.rb
@@ -1,7 +1,7 @@
 class Essh < Formula
   desc "Extended ssh command."
   homepage "https://essh.sitespread.net/"
-  version '2.0.0'
+  version '2.1.0'
   url "https://github.com/kohkimakimoto/essh/releases/download/v2.1.0/essh_darwin_amd64.zip"
   sha256 "56f4fb050c8fe330f1a52499c98cee578b726f24e2872e70d3afb09db794bb3c"
 
