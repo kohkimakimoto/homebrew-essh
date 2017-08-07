a3301e6ecab553f763e1613595fd2fe940d1b649
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-08-07 14:51:20 +0900

    update

--- essh.rb
+++ essh.rb
@@ -2,8 +2,8 @@ class Essh < Formula
   desc "Extended ssh command."
   homepage "https://essh.sitespread.net/"
   version '2.0.0'
-  url "https://github.com/kohkimakimoto/essh/releases/download/v2.0.0/essh_darwin_amd64.zip"
-  sha256 "e8f25916d911e8ab803b5964ac0ae7d6fc3e4abc28a9dfabcc94eaf1b981e92e"
+  url "https://github.com/kohkimakimoto/essh/releases/download/v2.1.0/essh_darwin_amd64.zip"
+  sha256 "56f4fb050c8fe330f1a52499c98cee578b726f24e2872e70d3afb09db794bb3c"
 
   def install
     bin.install 'essh'
