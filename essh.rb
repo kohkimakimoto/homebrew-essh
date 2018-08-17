3f858ee3f0cdce173b26620fbac598db3fc871ba
Kohki Makimoto <kohki.makimoto@gmail.com>
2018-08-17 17:06:11 +0900

    update to 2.3.0

--- essh.rb
+++ essh.rb
@@ -1,9 +1,9 @@
 class Essh < Formula
   desc "Extended ssh command."
   homepage "https://kohkimakimoto.github.io/essh/"
-  version '2.2.0'
-  url "https://github.com/kohkimakimoto/essh/releases/download/v2.2.0/essh_darwin_amd64.zip"
-  sha256 "ae2163ceacf91bb52b9efd3ecb06f6dc32acffbbf32155609aefb879cfa90cd6"
+  version '2.3.0'
+  url "https://github.com/kohkimakimoto/essh/releases/download/v2.3.0/essh_darwin_amd64.zip"
+  sha256 "90c56302fa16de74f10ad0b8a6d6eb874831469bf9144d7a2484d049b04f0efd"
 
   def install
     bin.install 'essh'
