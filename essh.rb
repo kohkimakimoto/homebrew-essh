373e53ffb418af324077fb2d72f28602f2f580a0
Kohki Makimoto <kohki.makimoto@gmail.com>
2019-03-29 12:33:20 +0900

    update version

--- essh.rb
+++ essh.rb
@@ -1,7 +1,7 @@
 class Essh < Formula
   desc "Extended ssh command."
   homepage "https://kohkimakimoto.github.io/essh/"
-  version '2.5.0'
+  version '3.1.0'
   url "https://github.com/kohkimakimoto/essh/releases/download/v3.1.0/essh_darwin_amd64.zip"
   sha256 "34677b3a4a646be92f3ed4860af4f06c0921075620ae11b64c65fa7c4322df8f"
 
